97acd8482ed1fc6f5f60a34f9ecd2255fb1c2bae
atrac17 <32810066+atrac17@users.noreply.github.com>
2022-07-26 10:52:01 -0400

    Armored Police Batrider (Update)
    1. Update Inputs (Flip Coin/Start Mapping/Test Switch)
    2. All DIPs verified working for batrider.xml
    3. Pause function from jtframe added to MRA
    4. MRA updates for mapping/dips (additional mame sets for alternatives / separate PR to @theypsilon)
    5. cfgstr updates (clean up and removed audio mixing options, currently not functional - I will attempt to pass them off in a future PR)
    6. Build Date added to cfgstr and batrider.qsf (need to figure out how to pass this without using go files to generate, will need to be updated manually - I will attempt to pass them off in a future PR)
    6. Updated batrider.def file for clarification and removed unnecessary entries
    7. Updated batrider.qsf file for clarification and removed unnecessary entries
    8. Changed SDRAM parameters, not familiar with SDRAM and this does not change timing constraints (see below)
    
    Tested several builds changing SDRAM parameters, if one of these is an issue please let me know. The following are stable for v2.9 128MB modules on three setups that previously failed ROM checks:
    
    ### SDRAM parameters
    set_global_assignment -name VERILOG_MACRO "JTFRAME_SDRAM96=1"
    set_global_assignment -name VERILOG_MACRO "JTFRAME_SDRAM_BANKS=1"
    set_global_assignment -name VERILOG_MACRO "JTFRAME_SDRAM_REPACK=1"
    set_global_assignment -name VERILOG_MACRO "JTFRAME_NOHOLDBUS=1"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA0_AUTOPRECH=1"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA1_AUTOPRECH=1"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA2_AUTOPRECH=1"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA3_AUTOPRECH=1"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA0_LEN=64"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA1_LEN=64"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA2_LEN=64"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_BA3_LEN=64"
    #set_global_assignment -name VERILOG_MACRO "JTFRAME_SDRAM_CHECK=1"

---
+++ rom/raizing.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+
+cd $JTUTIL/src
+make || exit $?
+cd -
+echo "------------"
+
+OUTDIR="mra"
+
+mkdir -p "$OUTDIR"
+mkdir -p "${OUTDIR}/$ALTD"
+
+#AUXTMP=/tmp/$RANDOM
+#jtcfgstr -target=mist -output=bash -def $DEF|grep _START > $AUXTMP
+#source $AUXTMP
+
+if [ ! -d xml ]; then
+  mkdir xml
+fi
+
+function mra {
+    local GAME=$1
+    local ALT=${2//[:]/}
+    local BUTSTR="$3"
+    local DIP="$4"
+
+    if [ ! -e xml/$GAME.xml ]; then
+        if [ ! -f $GAME.xml ]; then
+            mamefilter $GAME
+        fi
+        mv $GAME.xml xml/
+    fi
+
+    ALTD=_alt/_"$ALT"
+    mkdir -p $OUTDIR/"$ALTD"
+
+    echo -----------------------------------------------
+    echo "Dumping $GAME"
+    mame2dip xml/$GAME.xml -rbf batrider -outdir $OUTDIR -altfolder "$ALTD" \
+        -order maincpu audiocpu gp9001_0 oki1 oki2 \
+        -dipbase 8 \
+        -start maincpu    0x0       \
+        -start audiocpu   0x200000  \
+        -start gp9001_0   0x240000  \
+        -start oki1       0x1240000 \
+        -start oki2       0x1340000 \
+        -setword maincpu  16 \
+        -setword gp9001_0 16 reverse \
+        -frac 1 gp9001_0 2 \
+        -order-roms gp9001_0 0 2 1 3 \
+        -dipdef $DIP \
+        -corebuttons 3 \
+        -buttons $BUTSTR
+}
+
+mra  batrider "Armed Police Batrider"  "Shot,Bomb,Formation" "FC,00,E0,E1,FF,FF,FF,FF"
+
+function mra {
+    local GAME=$1
+    local ALT=${2//[:]/}
+    local BUTSTR="$3"
+    local DIP="$4"
+
+    if [ ! -e xml/$GAME.xml ]; then
+        if [ ! -f $GAME.xml ]; then
+            mamefilter $GAME
+        fi
+        mv $GAME.xml xml/
+    fi
+
+    ALTD=_alt/_"$ALT"
+    mkdir -p $OUTDIR/"$ALTD"
+
+    echo -----------------------------------------------
+    echo "Dumping $GAME"
+    mame2dip xml/$GAME.xml -rbf bakraid -outdir $OUTDIR -altfolder "$ALTD" \
+        -order maincpu audiocpu gp9001_0 oki1 oki2 \
+        -dipbase 8 \
+        -start maincpu    0x0       \
+        -start audiocpu   0x200000  \
+        -start gp9001_0   0x240000  \
+        -start oki1       0x1240000 \
+        -start oki2       0x1340000 \
+        -setword maincpu  16 \
+        -setword gp9001_0 16 reverse \
+        -frac 1 gp9001_0 2 \
+        -order-roms gp9001_0 0 2 1 3 \
+        -dipdef $DIP \
+        -corebuttons 3 \
+        -buttons $BUTSTR
+}
+
+mra  bbakraid "Battle Bakraid"         "Shot,Bomb,Formation" "00,00,00"
+
+function mra {
+    local GAME=$1
+    local ALT=${2//[:]/}
+    local BUTSTR="$3"
+    local DIP="$4"
+
+    if [ ! -e xml/$GAME.xml ]; then
+        if [ ! -f $GAME.xml ]; then
+            mamefilter $GAME
+        fi
+        mv $GAME.xml xml/
+    fi
+
+    ALTD=_alt/_"$ALT"
+    mkdir -p $OUTDIR/"$ALTD"
+
+    echo -----------------------------------------------
+    echo "Dumping $GAME"
+    mame2dip xml/$GAME.xml -rbf garegga -outdir $OUTDIR -altfolder "$ALTD" \
+        -order maincpu audiocpu gp9001_0 oki1 text \
+        -dipbase 8 \
+        -start maincpu    0x0       \
+        -start audiocpu   0x80000   \
+        -start gp9001_0   0x90000   \
+        -start oki1       0x290000  \
+        -start text       0x2D0000  \
+        -setword maincpu  16 \
+        -setword gp9001_0 16 reverse \
+        -frac 1 gp9001_0 2 \
+        -order-roms gp9001_0 0 1 \
+        -dipdef $DIP \
+        -corebuttons 3 \
+        -buttons $BUTSTR
+}
+
+mra  sstriker "Sorcer Striker"         "Shot,Bomb,Formation" "00,00,00"
+
+function mra {
+    local GAME=$1
+    local ALT=${2//[:]/}
+    local BUTSTR="$3"
+    local DIP="$4"
+
+    if [ ! -e xml/$GAME.xml ]; then
+        if [ ! -f $GAME.xml ]; then
+            mamefilter $GAME
+        fi
+        mv $GAME.xml xml/
+    fi
+
+    ALTD=_alt/_"$ALT"
+    mkdir -p $OUTDIR/"$ALTD"
+
+    echo -----------------------------------------------
+    echo "Dumping $GAME"
+    mame2dip xml/$GAME.xml -rbf garegga -outdir $OUTDIR -altfolder "$ALTD" \
+        -order maincpu audiocpu gp9001_0 oki1 text \
+        -dipbase 8 \
+        -start maincpu    0x0       \
+        -start audiocpu   0x100000  \
+        -start gp9001_0   0x110000  \
+        -start oki1       0x510000  \
+        -start text       0x590000  \
+        -setword maincpu  16 \
+        -setword gp9001_0 16 reverse \
+        -frac 1 gp9001_0 2 \
+        -order-roms gp9001_0 0 1 \
+        -dipdef $DIP \
+        -corebuttons 3 \
+        -buttons $BUTSTR
+}
+
+mra  kingdmgp "Kingdom Grandprix"      "Shot,Bomb,Formation" "00,00,00"
+
+function mra {
+    local GAME=$1
+    local ALT=${2//[:]/}
+    local BUTSTR="$3"
+    local DIP="$4"
+
+    if [ ! -e xml/$GAME.xml ]; then
+        if [ ! -f $GAME.xml ]; then
+            mamefilter $GAME
+        fi
+        mv $GAME.xml xml/
+    fi
+
+    ALTD=_alt/_"$ALT"
+    mkdir -p $OUTDIR/"$ALTD"
+
+    echo -----------------------------------------------
+    echo "Dumping $GAME"
+    mame2dip xml/$GAME.xml -rbf garegga -outdir $OUTDIR -altfolder "$ALTD" \
+        -nobootlegs \
+        -order maincpu audiocpu gp9001_0 oki1 text \
+        -dipbase 8 \
+        -start maincpu    0x0       \
+        -start audiocpu   0x100000  \
+        -start gp9001_0   0x120000  \
+        -start oki1       0x520000  \
+        -start text       0x620000  \
+        -setword maincpu  16 \
+        -setword gp9001_0 16 reverse \
+        -frac 1 gp9001_0 2 \
+        -order-roms gp9001_0 0 1 \
+        -dipdef $DIP \
+        -corebuttons 3 \
+        -buttons $BUTSTR
+}
+
+mra  bgaregga "Battle Garegga"         "Shot,Bomb,Formation" "00,00,00"
+
+exit 0
